21d1dbf890628adbf0191b3207808efa3d3f6c6a
joakinrg <jrojasg2100@alumno.ipn.mx>
2023-03-20 18:06:45 -0600

    delete

--- js/js.js
+++
@@ -1,7 +0,0 @@
-// Crea un elemento de párrafo en HTML
-const paragraph = document.createElement("p");
-
-// Agrega un texto al párrafo
-const text = document.createTextNode("Hola, mundo!");
-paragraph.appendChild(text);
-
